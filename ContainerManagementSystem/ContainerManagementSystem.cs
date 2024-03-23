57440406bc1d827ee8bc8cb02dfc1bedd373b7ae
HubiRto <hubert.rybicki.hr@gmail.com>
2024-03-23 22:09:36 +0100

    End

--- ContainerManagementSystem/ContainerManagementSystem.cs
+++ ContainerManagementSystem/ContainerManagementSystem.cs
@@ -2,5 +2,147 @@
 
 public class ContainerManagementSystem
 {
+    public abstract class Container
+    {
+        public string SerialNumber { get; set; }
+        public double LoadCapacity { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public double Depth { get; set; }
+        public double CurrentLoad { get; protected set; }
+
+        public abstract void LoadCargo(double cargoWeight);
+        public abstract void UnloadCargo();
+        public string GetInfo()
+        {
+            return $"Numer seryjny: {SerialNumber}, Masa ładunku: {Weight} kg";
+        }
+    }
+
+    public class LiquidContainer : Container
+    {
+        public double MaxCapacity { get; set; }
+
+        public override void LoadCargo(double cargoWeight)
+        {
+            if (cargoWeight > MaxCapacity)
+                throw new OverfillException();
+
+            CurrentLoad += cargoWeight;
+        }
+
+        public override void UnloadCargo()
+        {
+            CurrentLoad = 0.05 * MaxCapacity;
+        }
+    }
     
+    public class GasContainer : Container, IHazardNotifier
+    {
+        public double Pressure { get; set; }
+
+        public override void LoadCargo(double cargoWeight)
+        {
+            if (cargoWeight > LoadCapacity)
+                throw new OverfillException();
+
+            CurrentLoad += cargoWeight;
+        }
+
+        public override void UnloadCargo()
+        {
+            CurrentLoad *= 0.95;
+        }
+
+        public void NotifyDanger(string containerNumber)
+        {
+            Console.WriteLine($"Container {containerNumber} has encountered a hazardous situation.");
+        }
+    }
+    
+    public class RefrigeratedContainer : Container
+    {
+        public string ProductType { get; set; }
+        public double Temperature { get; set; }
+
+        public override void LoadCargo(double cargoWeight)
+        {
+            if (cargoWeight > LoadCapacity)
+                throw new OverfillException();
+
+            CurrentLoad += cargoWeight;
+        }
+
+        public override void UnloadCargo()
+        {
+            CurrentLoad = 0;
+        }
+    }
+    
+    public interface IHazardNotifier
+    {
+        void NotifyDanger(string containerNumber);
+    }
+    
+    public class Ship
+    {
+        public List<Container> Containers { get; set; }
+        public double MaxLoadWeight { get; set; }
+
+        public Ship()
+        {
+            Containers = new List<Container>();
+        }
+
+        public void LoadContainer(Container container)
+        {
+            double currentLoadWeight = 0;
+            foreach (var c in Containers)
+            {
+                currentLoadWeight += c.CurrentLoad;
+            }
+
+            if (currentLoadWeight + container.CurrentLoad > MaxLoadWeight)
+                throw new OverloadException();
+
+            Containers.Add(container);
+        }
+
+        public void UnloadContainer(Container container)
+        {
+            Containers.Remove(container);
+        }
+        public void ReplaceContainer(Container oldContainer, Container newContainer)
+        {
+            int index = Containers.IndexOf(oldContainer);
+            if (index != -1)
+            {
+                Containers[index] = newContainer;
+            }
+        }
+        public string GetInfo()
+        {
+            string info = $"Maksymalna waga statku: {MaxLoadWeight} kg\n";
+            info += "Lista kontenerów na statku:\n";
+            foreach (var container in Containers)
+            {
+                info += container.GetInfo() + "\n";
+            }
+            return info;
+        }
+    }
+    
+    public class OverloadException : Exception
+    {
+        public OverloadException() : base("Ship overload!")
+        {
+        }
+    }
+    
+    public class OverfillException : Exception
+    {
+        public OverfillException() : base("Container overfill!")
+        {
+        }
+    }
 }
--- ContainerManagementSystem/Program.cs
+++ ContainerManagementSystem/Program.cs
@@ -1,2 +1,223 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Witaj w aplikacji do zarządzania załadunkiem kontenerów!");
+        ContainerManagementSystem.ContainerManagementSystem.Ship ship =
+            new ContainerManagementSystem.ContainerManagementSystem.Ship();
+        ship.MaxLoadWeight = 10000;
+        
+        while (true)
+        {
+            Console.WriteLine("\nWybierz opcję:");
+            Console.WriteLine("1. Stworzenie kontenera danego typu");
+            Console.WriteLine("2. Załadowanie ładunku do danego kontenera");
+            Console.WriteLine("3. Załadowanie kontenera na statek");
+            Console.WriteLine("4. Załadowanie listy kontenerów na statek");
+            Console.WriteLine("5. Usunięcie kontenera ze statku");
+            Console.WriteLine("6. Rozładowanie kontenera");
+            Console.WriteLine("7. Zastąpienie kontenera na statku o danym numerze innym kontenerem");
+            Console.WriteLine("8. Możliwość przeniesienie kontenera między dwoma statkami");
+            Console.WriteLine("9. Wypisanie informacji o danym kontenerze");
+            Console.WriteLine("10. Wypisanie informacji o danym statku i jego ładunku");
+            Console.WriteLine("11. Zakończ program");
+
+            int option;
+            if (!int.TryParse(Console.ReadLine(), out option))
+            {
+                Console.WriteLine("Wprowadzono nieprawidłową opcję.");
+                continue;
+            }
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Wybierz typ kontenera (L - na płyny, G - na gaz, C - chłodniczy):");
+                    string containerType = Console.ReadLine().ToUpper();
+                    ContainerManagementSystem.ContainerManagementSystem.Container container;
+                    switch (containerType)
+                    {
+                        case "L":
+                            container = new ContainerManagementSystem.ContainerManagementSystem.LiquidContainer();
+                            break;
+                        case "G":
+                            container = new ContainerManagementSystem.ContainerManagementSystem.GasContainer();
+                            break;
+                        case "C":
+                            container = new ContainerManagementSystem.ContainerManagementSystem.RefrigeratedContainer();
+                            break;
+                        default:
+                            Console.WriteLine("Nieznany typ kontenera.");
+                            continue;
+                    }
+
+                    Console.WriteLine("Podaj masę ładunku:");
+                    double cargoWeight;
+                    if (!double.TryParse(Console.ReadLine(), out cargoWeight))
+                    {
+                        Console.WriteLine("Wprowadzono nieprawidłową masę ładunku.");
+                        continue;
+                    }
+
+                    container.LoadCargo(cargoWeight);
+                    Console.WriteLine($"Kontener utworzony: {container.SerialNumber}");
+                    break;
+                case 2:
+                    Console.WriteLine("Podaj numer seryjny kontenera:");
+                    string containerNumber = Console.ReadLine();
+                    Console.WriteLine("Podaj masę ładunku:");
+                    double cargoWeight2;
+                    if (!double.TryParse(Console.ReadLine(), out cargoWeight2))
+                    {
+                        Console.WriteLine("Wprowadzono nieprawidłową masę ładunku.");
+                        continue;
+                    }
+                    
+                    ContainerManagementSystem.ContainerManagementSystem.Container selectedContainer =
+                        ship.Containers.Find(c => c.SerialNumber == containerNumber);
+                    if (selectedContainer == null)
+                    {
+                        Console.WriteLine("Nie znaleziono kontenera o podanym numerze.");
+                        continue;
+                    }
+
+                    selectedContainer.LoadCargo(cargoWeight2);
+                    Console.WriteLine($"Ładunek załadowany do kontenera {containerNumber}.");
+                    break;
+                case 3:
+                    Console.WriteLine("Podaj numer seryjny kontenera:");
+                    string containerNumber3 = Console.ReadLine();
+                    ContainerManagementSystem.ContainerManagementSystem.Container selectedContainer3 =
+                        ship.Containers.Find(c => c.SerialNumber == containerNumber3);
+                    if (selectedContainer3 == null)
+                    {
+                        Console.WriteLine("Nie znaleziono kontenera o podanym numerze.");
+                        continue;
+                    }
+
+                    try
+                    {
+                        ship.LoadContainer(selectedContainer3);
+                        Console.WriteLine($"Kontener {containerNumber3} załadowany na statek.");
+                    }
+                    catch (ContainerManagementSystem.ContainerManagementSystem.OverloadException)
+                    {
+                        Console.WriteLine("Przekroczono maksymalną wagę statku!");
+                    }
+
+                    break;
+                case 4:
+                    Console.WriteLine("Podaj ilość kontenerów do załadowania:");
+                    int count;
+                    if (!int.TryParse(Console.ReadLine(), out count) || count < 1)
+                    {
+                        Console.WriteLine("Wprowadzono nieprawidłową ilość kontenerów.");
+                        continue;
+                    }
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.WriteLine($"Kontener {i + 1}:");
+                        Console.WriteLine("Podaj numer seryjny kontenera:");
+                        string containerNumber4 = Console.ReadLine();
+                        ContainerManagementSystem.ContainerManagementSystem.Container selectedContainer4 =
+                            ship.Containers.Find(c => c.SerialNumber == containerNumber4);
+                        if (selectedContainer4 == null)
+                        {
+                            Console.WriteLine(
+                                $"Nie znaleziono kontenera o numerze {containerNumber4}. Kontynuowanie.");
+                            continue;
+                        }
+
+                        try
+                        {
+                            ship.LoadContainer(selectedContainer4);
+                            Console.WriteLine($"Kontener {containerNumber4} załadowany na statek.");
+                        }
+                        catch (ContainerManagementSystem.ContainerManagementSystem.OverloadException)
+                        {
+                            Console.WriteLine("Przekroczono maksymalną wagę statku! Kontynuowanie.");
+                        }
+                    }
+
+                    break;
+                case 5:
+                    Console.WriteLine("Podaj numer seryjny kontenera:");
+                    string containerNumber5 = Console.ReadLine();
+                    ContainerManagementSystem.ContainerManagementSystem.Container selectedContainer5 =
+                        ship.Containers.Find(c => c.SerialNumber == containerNumber5);
+                    if (selectedContainer5 == null)
+                    {
+                        Console.WriteLine("Nie znaleziono kontenera o podanym numerze.");
+                        continue;
+                    }
+
+                    ship.UnloadContainer(selectedContainer5);
+                    Console.WriteLine($"Kontener {containerNumber5} usunięty ze statku.");
+                    break;
+                case 6:
+                    Console.WriteLine("Podaj numer seryjny kontenera:");
+                    string containerNumber6 = Console.ReadLine();
+                    ContainerManagementSystem.ContainerManagementSystem.Container selectedContainer6 = ship.Containers.Find(c => c.SerialNumber == containerNumber6);
+                    if (selectedContainer6 == null)
+                    {
+                        Console.WriteLine("Nie znaleziono kontenera o podanym numerze.");
+                        continue;
+                    }
+
+                    ship.UnloadContainer(selectedContainer6);
+                    Console.WriteLine($"Kontener {containerNumber6} rozładowany.");
+                    break;
+                case 7:
+                    Console.WriteLine("Podaj numer seryjny kontenera na statku:");
+                    string oldContainerNumber7 = Console.ReadLine();
+                    ContainerManagementSystem.ContainerManagementSystem.Container oldContainer7 = ship.Containers.Find(c => c.SerialNumber == oldContainerNumber7);
+                    if (oldContainer7 == null)
+                    {
+                        Console.WriteLine("Nie znaleziono kontenera o podanym numerze na statku.");
+                        continue;
+                    }
+
+                    Console.WriteLine("Podaj numer seryjny nowego kontenera:");
+                    string newContainerNumber7 = Console.ReadLine();
+                    ContainerManagementSystem.ContainerManagementSystem.Container newContainer7 = ship.Containers.Find(c => c.SerialNumber == newContainerNumber7);
+                    if (newContainer7 == null)
+                    {
+                        Console.WriteLine("Nie znaleziono nowego kontenera o podanym numerze.");
+                        continue;
+                    }
+
+                    ship.ReplaceContainer(oldContainer7, newContainer7);
+                    Console.WriteLine(
+                        $"Kontener {oldContainerNumber7} został zastąpiony przez kontener {newContainerNumber7}.");
+                    break;
+                case 8:
+                    Console.WriteLine(
+                        "Funkcja przenoszenia kontenera między dwoma statkami w trakcie implementacji...");
+                    break;
+                case 9:
+                    Console.WriteLine("Podaj numer seryjny kontenera:");
+                    string containerNumber9 = Console.ReadLine();
+                    ContainerManagementSystem.ContainerManagementSystem.Container selectedContainer9 =
+                        ship.Containers.Find(c => c.SerialNumber == containerNumber9);
+                    if (selectedContainer9 == null)
+                    {
+                        Console.WriteLine("Nie znaleziono kontenera o podanym numerze.");
+                        continue;
+                    }
+
+                    Console.WriteLine(selectedContainer9.GetInfo());
+                    break;
+                case 10:
+                    Console.WriteLine(ship.GetInfo());
+                    break;
+                case 11:
+                    Console.WriteLine("Zakończono program.");
+                    return;
+                default:
+                    Console.WriteLine("Wybrano nieprawidłową opcję.");
+                    break;
+            }
+        }
+    }
+}
